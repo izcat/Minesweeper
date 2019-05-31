f3446baea8be6124b973f6724c2a9fa47dc9fe4e
ZYCat <32667939+izcat@users.noreply.github.com>
2019-05-31 14:53:36 +0800

    Create Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+import calc.Calculator;
+//import game.Game;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		new Calculator();
+	//	new Game();
+	}
+}
+
